0c9c348aff63040a9a11858c0d47e4a5a9607762
Evan Power <evpower7@gmail.com>
2018-03-27 02:39:49 +0100

    Broke it

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-
 import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
-import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
+import MenuIcon from 'material-ui-icons/Menu';
 
 import Avatar from 'material-ui/Avatar';
 import github from './icons/github.png';
 import linkedin from './icons/linkedin.png';
 
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const style = {
+  title: {
+    flex: 1,
+  },
+  navigationTitle: {
+    minWidth: 200,
+  },
+};
 
 class App extends Component {
   render() {
-    const rightIcons = (
-      <div>
-        <FlatButton
-        href="https://github.com/PowerTaxi"
-        target="_blank">
-        <Avatar src ={github}/>
-        </FlatButton>
-        <FlatButton
-        href="https://www.linkedin.com/in/evanpower7/"
-        target="_blank">
-        <Avatar src ={linkedin}/>
-        </FlatButton>
-      </div>
-    );
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
       <div className="App">
-      {/*   <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        
-        <AppBar position = "static"
-          titleStyle={{textAlign:'left'}}
-          title = "Evan Power"
-          iconElementRight={rightIcons}
-      />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
+
+        <AppBar position= "static">
+          <Toolbar>
+            <Typography type="title" color="inherit" style={style.title}>
+              Evan Power
+            </Typography>
+            <IconButton color="contrast" aria-label="Menu" href="https://github.com/PowerTaxi"
+              target="_blank">
+            <Avatar src ={github}/>
+            </IconButton>
+            <IconButton color="contrast" aria-label="Menu" href="https://www.linkedin.com/in/evanpower7/"
+              target="_blank">
+              <Avatar src ={linkedin}/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
       </div>
-      </MuiThemeProvider>
     );
   }
 }
