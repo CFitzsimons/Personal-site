ae5cbe6468338303fbdb4074656bc9d7927fe680
Evan Power <evpower7@gmail.com>
2018-03-29 00:53:46 +0100

    broke it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar color = "warning" light>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <NavbarBrand href="/" className="mr-auto">Evan Power</NavbarBrand>
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">CV</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/components/">Projects</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/PowerTaxi">GitHub</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://linkedin.com/en/evpower7">LinkedIn</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+
       </div>
     );
   }
